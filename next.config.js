ea2c5792730e9dc1605a9ef5c36e386c68b3c1cf
Sayman Lal <buisnesssayman@gmail.com>
2025-07-22 07:47:05 -0400

    scaled the texts

--- next.config.js
+++ next.config.js
@@ -41,3 +41,13 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = {
+  // ... existing config
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  }
+}
